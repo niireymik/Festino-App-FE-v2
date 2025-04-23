0bfeb68fb72eec7c5c44a809a4db4aacbf276bbd
김민지 <alswlk0306@gmail.com>
2025-04-24 03:13:24 +0900

    style: 라우팅을 위한 페이지 생성 및 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,16 @@ import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-primary-900">
-      <h1 className="text-3xl font-bold text-white">Tailwind is working!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/timetable" element={<TimeTablePage />} />
+          <Route path="/booth" element={<BoothPage />} />
+          <Route path="tabling" element={<TablingPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/mains/HomePage.tsx
@@ -0,0 +1,7 @@
+const HomePage : React.FC = () => {
+  return (
+    <></>
+  )
+}
+
+export default HomePage;
