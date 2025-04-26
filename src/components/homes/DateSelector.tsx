6fbcc0551264d3304057ebd0a781413f03132abf
jzongyun <jeong.yun@tukorea.ac.kr>
2025-04-27 00:52:00 +0900

    style: border의 사이즈 변경

--- src/components/homes/DateSelector.tsx
+++ src/components/homes/DateSelector.tsx
@@ -4,14 +4,14 @@ const DateSelector: React.FC = () => {
   const { festivalDate, setDate } = useDateStore();
 
   return (
-    <div className="flex px-5 justify-between z-50 select-none">
+    <div className="flex px-5 pt-1 justify-between z-50 select-none">
       {[1, 2, 3].map((date) => (
         <div key={date}>
           <div
             className={`flex justify-center gap-2 w-[105px] xs:gap-4 xs:w-[122px] sm:gap-5 sm:w-[140px] py-2.5 px-0.5 rounded-full shadow-4xl text-xs items-center cursor-pointer ${
               festivalDate === date
                  ? 'bg-primary-700 text-white font-bold'
-                 : 'text-primary-700-light font-normal border-primary-900-light-16 border'
+                 : 'text-primary-700-light font-normal border-primary-900-light-16 border-1'
              }`}
              onClick={() => setDate(date)}
           >
