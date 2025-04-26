c3a633c5b08531a7773d774c5c2b929540fd9ca3
김민지 <alswlk0306@gmail.com>
2025-04-27 05:49:29 +0900

    style: 주문하기 페이지 레이아웃 추가 및 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,25 @@ import BoothPage from './pages/mains/BoothPage';
 import TablingPage from './pages/mains/TablingPage';
 import MainLayout from './layouts/MainLayout';
 import NotificationPage from './pages/mains/NotificationPage';
+import OrderLayout from './layouts/OrderLayout';
+import OrderHomePage from './pages/orders/OrderHomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/timetable" element={<TimeTablePage />} />
-          <Route path="/booth" element={<BoothPage />} />
-          <Route path="/reserve" element={<TablingPage />} />
-          <Route path="/notification" element={<NotificationPage />} />
+        {/* Main */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="timetable" element={<TimeTablePage />} />
+          <Route path="booth" element={<BoothPage />} />
+          <Route path="reserve" element={<TablingPage />} />
+          <Route path="notification" element={<NotificationPage />} />
+        </Route>
+
+        {/* Order */}
+        <Route path="/order" element={<OrderLayout />}>
+          <Route path=":boothId/:tableNum" element={<OrderHomePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
