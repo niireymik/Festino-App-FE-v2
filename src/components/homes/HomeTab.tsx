6a6dfd65eadc7a3fdc4f1d2322709113058c5512
jzongyun <jeong.yun@tukorea.ac.kr>
2025-04-26 22:45:09 +0900

    feat: 홈 페이지 행사 날짜 버튼 퍼블리싱

---
+++ src/components/homes/HomeTab.tsx
@@ -0,0 +1,11 @@
+import DateSelector from "./DateSelector";
+
+const HomeTab: React.FC = () => {
+  return (
+    <div>
+      <DateSelector></DateSelector>
+    </div>
+  );
+};
+
+export default HomeTab;
--- src/pages/mains/HomePage.tsx
+++ src/pages/mains/HomePage.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/headers/Header";
 import HomeBanner from "@/components/homes/HomeBanner";
+import HomeTab from "@/components/homes/HomeTab";
 
 const HomePage : React.FC = () => {
   return (
     <>
       <Header />
       <HomeBanner />
+      <HomeTab />
     </>
   )
 }
