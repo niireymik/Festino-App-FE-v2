897c10969fc12a19c197622774262b109b93e154
Hyochan <hyochan02@gmail.com>
2025-04-29 13:40:39 +0900

    style: 팀-리뷰 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './pages/mains/HomePage';
 import TimeTablePage from './pages/mains/TimeTablePage';
 import BoothPage from './pages/mains/BoothPage';
 import TablingPage from './pages/mains/TablingPage';
+import TeamReviewPage from './pages/mains/TeamReviewPage';
 import MainLayout from './layouts/MainLayout';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/timetable" element={<TimeTablePage />} />
           <Route path="/booth" element={<BoothPage />} />
           <Route path="/reserve" element={<TablingPage />} />
+          <Route path="/teamreview" element={<TeamReviewPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/mains/TeamReviewPage.tsx
@@ -0,0 +1,5 @@
+const TeamReviewPage: React.FC = () => {
+  return <></>;
+};
+
+export default TeamReviewPage;
