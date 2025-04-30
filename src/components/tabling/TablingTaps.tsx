e647f61e648d96494a5172b790a55e0c758805e2
Hyochan <hyochan02@gmail.com>
2025-04-30 17:34:03 +0900

    style: 예약 페이지 배너, 탭 컴포넌트화화

---
+++ src/components/tabling/TablingTaps.tsx
@@ -0,0 +1,5 @@
+const TablingTaps: React.FC = () => {
+  return <></>;
+};
+
+export default TablingTaps;
--- src/pages/mains/TablingPage.tsx
+++ src/pages/mains/TablingPage.tsx
@@ -1,7 +1,15 @@
-const TablingPage : React.FC = () => {
+import TablingBanner from '@/components/tabling/TablingBanner';
+import TablingTaps from '@/components/tabling/TablingTaps';
+
+const TablingPage: React.FC = () => {
   return (
-    <></>
-  )
-}
+    <>
+      <div className="min-h-screen flex flex-col relative">
+        <TablingBanner />
+        <TablingTaps />
+      </div>
+    </>
+  );
+};
 
 export default TablingPage;
