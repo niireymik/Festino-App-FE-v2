26606ea5757c0989e70f29defae2f38e01ec1218
Hyochan <hyochan02@gmail.com>
2025-05-06 01:26:04 +0900

    chore: 예약 페이지 배너 추가, border 사이즈 10xl 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -112,10 +112,13 @@ export default {
         'default': "url('/images/booths/default.svg')",
         'arrow-back-white': "url('/icons/booths/arrow-back.svg')",
         'arrow-forward': "url('/icons/booths/arrow-forward.svg')",
-        'reservation-status': "url('/images/booths/reserve.svg')"
+        'reservation-status': "url('/images/booths/reserve.svg')",
+        // tabling
+        'tabling-banner': "url('/images/tabling/tino-tabling.svg')"
       },
       borderRadius: {
         "2.5xl": "1.25rem", // 20px
+        "10xl": '3.125rem', // 50px
       },
       borderColor: (theme) => ({
         ...theme('colors'),
