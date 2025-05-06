e90ef8f49cce57d71fc92b327e357b3aaf8db647
Hyochan <hyochan02@gmail.com>
2025-05-07 01:52:07 +0900

    style: LoadingModal 퍼블리싱

--- src/components/tabling/modals/LoadingModal.tsx
+++ src/components/tabling/modals/LoadingModal.tsx
@@ -1,15 +1,13 @@
 const LoadingModal: React.FC = () => {
   return (
-    <>
-      <div className="relative col-start-2 row-start-2 h-full dynamic-width flex flex-col items-center gap-7">
-        <img src="/icons/orders/loading-tino.svg" className="w-[206px]" />
-        <div className="text-white font-medium text-center">
-          귀여운 티노가
-          <br />
-          메세지를 보내고 있어요
-        </div>
+    <div className="relative col-start-2 row-start-2 h-full dynamic-width flex flex-col items-center gap-7">
+      <img src="/icons/orders/loading-tino.svg" className="w-[206px]" />
+      <div className="text-white font-medium text-center">
+        귀여운 티노가
+        <br />
+        메세지를 보내고 있어요
       </div>
-    </>
+    </div>
   );
 };
 
