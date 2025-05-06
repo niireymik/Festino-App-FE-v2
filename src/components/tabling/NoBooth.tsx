d0bb40a215688eca0abc755f5d9ee400cc9b9465
Hyochan <hyochan02@gmail.com>
2025-05-07 01:48:11 +0900

    style: 예약 부스 없음 페이지 퍼블리싱

--- src/components/tabling/NoBooth.tsx
+++ src/components/tabling/NoBooth.tsx
@@ -1,9 +1,9 @@
 const NoBooth: React.FC = () => {
   return (
     <>
-      <div className="w-full flex flex-col items-center gap-2 py-2">
+      <div className="w-full h-full flex flex-col items-center gap-2 py-2">
         <p className="text-xl font-medium">예약 가능한 부스가 없습니다.</p>
-        <div className="bg-error-full bg-cover bg-center bg-no-repeat w-full aspect-[35/26]"></div>
+        <div className="bg-error-full bg-cover bg-center bg-no-repeat w-full aspect-[35/31]"></div>
       </div>
       ;
     </>
