c37541adf03b3107261a66ff7a79b80d6c4d7c02
Hyochan <hyochan02@gmail.com>
2025-05-07 02:02:18 +0900

    style: 이름, 인원수, 전화번호 입력 컴포넌트 구현

---
+++ src/components/tabling/InputName.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+interface InputNameProps {
+  value: string;
+  onChange: (val: string) => void;
+}
+
+const InputName: React.FC<InputNameProps> = ({ value, onChange }) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let filtered = e.target.value.replace(/[^a-zA-Z0-9ㄱ-ㅎ가-힣 ]/g, '');
+    if (filtered.length > 5) {
+      filtered = filtered.slice(0, 5);
+    }
+    onChange(filtered);
+  };
+
+  return (
+    <>
+      <div className="text-xs">이름</div>
+      <div className="h-11 w-full flex flex-row items-center py-2.5 gap-2.5">
+        <img src="/icons/person.svg" className="w-6 h-6" />
+        <input
+          className="flex-1 focus:outline-none bg-inherit"
+          value={value}
+          placeholder="티노"
+          type="text"
+          onChange={handleInput}
+          maxLength={5}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </div>
+      <hr className={`mb-[30px] border-0 h-[1px] ${isFocused ? 'bg-primary-900' : 'bg-secondary-500-light-20'}`} />
+    </>
+  );
+};
+
+export default InputName;
