37eb6438e48687de9b873183eafc4d437fda02ae
Hyochan <hyochan02@gmail.com>
2025-05-08 23:23:07 +0900

    style: noReserveModal 추가

--- src/pages/mains/ModalPage.tsx
+++ src/pages/mains/ModalPage.tsx
@@ -1,15 +1,17 @@
-import useBaseModal from "@/stores/baseModal";
-import ModalBackground from "@/components/modals/ModalBackground";
-import TimetableModal from "@/components/homes/TimetableModal"
+import useBaseModal from '@/stores/baseModal';
+import ModalBackground from '@/components/modals/ModalBackground';
+import TimetableModal from '@/components/homes/TimetableModal';
+import NoReserveModal from '@/components/tabling/modals/NoReserveModal';
 
 const ModalPage = () => {
   const { isModalOpen, modalType } = useBaseModal();
-  
+
   if (!isModalOpen) return null;
 
   return (
     <ModalBackground>
-      {modalType === "timetable" && <TimetableModal />}
+      {modalType === 'timetable' && <TimetableModal />}
+      {modalType === 'noReserveModal' && <NoReserveModal />}
     </ModalBackground>
   );
 };
