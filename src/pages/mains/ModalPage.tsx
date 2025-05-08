906f12f6f17f9c9d9d3c4a496c5b5bdf755e4319
Hyochan <hyochan02@gmail.com>
2025-05-08 23:30:00 +0900

    style: 예약 조회 관련 모달 연결

--- src/pages/mains/ModalPage.tsx
+++ src/pages/mains/ModalPage.tsx
@@ -2,6 +2,11 @@ import useBaseModal from '@/stores/baseModal';
 import ModalBackground from '@/components/modals/ModalBackground';
 import TimetableModal from '@/components/homes/TimetableModal';
 import NoReserveModal from '@/components/tabling/modals/NoReserveModal';
+import LoadingModal from '@/components/tabling/modals/LoadingModal';
+import FailReservationModal from '@/components/tabling/modals/FailReservationModal';
+import EnterBoothModal from '@/components/tabling/modals/EnterBoothModal';
+import SearchReservationModal from '@/components/tabling/modals/SearchReservationModal';
+import DuplicateModal from '@/components/tabling/modals/DuplicateModal';
 
 const ModalPage = () => {
   const { isModalOpen, modalType } = useBaseModal();
@@ -11,7 +16,12 @@ const ModalPage = () => {
   return (
     <ModalBackground>
       {modalType === 'timetable' && <TimetableModal />}
+      {modalType === 'loadingModal' && <LoadingModal />}
+      {modalType === 'failReservationModal' && <FailReservationModal />}
+      {modalType === 'enterBoothModal' && <EnterBoothModal />}
+      {modalType === 'searchReservationModal' && <SearchReservationModal />}
       {modalType === 'noReserveModal' && <NoReserveModal />}
+      {modalType === 'duplicateModal' && <DuplicateModal />}
     </ModalBackground>
   );
 };
