a719f028c7cec8a829e9a3f1417438886d2cf227
jzongyun <jeong.yun@tukorea.ac.kr>
2025-05-10 21:14:59 +0900

    refactor: '를 "로 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -121,10 +121,6 @@ export default {
         'timetable-banner': "url('/images/timetables/timetable-banner.svg')",
         // tabling
         'tabling-banner': "url('/images/tablings/tino-tabling.svg')",
-        // timetable
-        'timetable-banner': "url('/images/timetables/timetable-banner.svg')",
-        // tabling
-        'tabling-banner': "url('/images/tablings/tino-tabling.svg')",
       },
       borderRadius: {
         "2.5xl": "1.25rem", // 20px
