ff6d0495cfde943882791fffc5ad662975344661
jzongyun <jeong.yun@tukorea.ac.kr>
2025-05-10 23:47:14 +0900

    style: 공연 정보 상세 조회 버튼 색상 변경

--- src/components/timetable/TimeTableDetail.tsx
+++ src/components/timetable/TimeTableDetail.tsx
@@ -15,8 +15,13 @@ const TimeTableDetail: React.FC<ClubProps> = ({ club }) => {
     ? 'border-primary text-primary-700'
     : 'bg-secondary-50 border-secondary-100 text-secondary-100';
 
+  const isShowingButton = club.isShowing
+    ? 'bg-primary-700'
+    : 'bg-secondary-300'
+
   return (
     <div
+      onClick={() => openTimetableModal(club)}
       className={`cursor-pointer flex py-5 rounded-3xl w-[170px] xs:w-[210px] sm:w-[230px] border-2 flex-col items-center gap-2.5 shadow-4xl ${isShowing}`}
     >
       <div
@@ -28,8 +33,7 @@ const TimeTableDetail: React.FC<ClubProps> = ({ club }) => {
         <div>' {club.performer} '</div>
       </div>
       <button
-        onClick={() => openTimetableModal(club)}
-        className="text-white bg-primary-700 w-[120px] h-[30px] rounded-full text-xs"
+        className={`text-white w-[120px] h-[30px] rounded-full text-xs ${isShowingButton}`}
       >
         공연 정보 상세보기
       </button>
