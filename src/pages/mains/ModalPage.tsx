7fbb6affb4e3ee4c34acab1cee253208bb0e4e8b
Hyochan <hyochan02@gmail.com>
2025-05-12 20:56:37 +0900

    chore: 로그인 모달 추가

--- src/pages/mains/ModalPage.tsx
+++ src/pages/mains/ModalPage.tsx
@@ -10,6 +10,7 @@ import DuplicateModal from '@/components/tablings/modals/DuplicateModal';
 import ReservationModal from '@/components/tablings/modals/ReservationModal';
 import CompleteReserveModal from '@/components/tablings/modals/CompleteReserveModal';
 import MessageFailModal from '@/components/tablings/modals/MessageFailModal';
+import LoginModal from '@/components/auths/LoginModal';
 
 const ModalPage = () => {
   const { isModalOpen, modalType } = useBaseModal();
@@ -28,6 +29,7 @@ const ModalPage = () => {
       {modalType === 'failReservationModal' && <FailReservationModal />}
       {modalType === 'completeReserveModal' && <CompleteReserveModal />}
       {modalType === 'messageFailModal' && <MessageFailModal />}
+      {modalType === 'loginModal' && <LoginModal />}
     </ModalBackground>
   );
 };
