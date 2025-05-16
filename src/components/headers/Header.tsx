151fcb3c2bbf0c24ff20763d89402228a6a1c7fd
Hyochan <hyochan02@gmail.com>
2025-05-16 14:06:40 +0900

    style: 네비게이션 탭 토글 이벤트 추가

--- src/components/headers/Header.tsx
+++ src/components/headers/Header.tsx
@@ -1,9 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import useBaseModal from '@/stores/baseModal';
+import useNavTapStore from '@/stores/headers/navTapStore';
 
 const Header: React.FC = () => {
   const { openModal } = useBaseModal();
   const navigate = useNavigate();
+  const { toggle } = useNavTapStore();
 
   const handleClickMainSymbol = () => {
     navigate('/', { replace: true });
@@ -13,15 +15,11 @@ const Header: React.FC = () => {
     openModal('loginModal');
   };
 
-  const handleClickNavSymbol = () => {
-    // 대충 네비게이션 바 나오게 하면 됨
-  };
-
   return (
     <div className="w-full h-[58px] flex flex-row bg-white justify-between px-5 items-center">
       <div
         className="w-[22px] h-[22px] bg-header-navigation-bar bg-center bg-no-repeat bg-[length:22px_22px] cursor-pointer"
-        onClick={handleClickNavSymbol}
+        onClick={toggle}
       />
       <div
         className="w-[68px] h-[36px] bg-header-festino-logo bg-center bg-no-repeat bg-[length:68px_36px] cursor-pointer"
