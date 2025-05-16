eb01679a82528e74ba58ef17cf3b815da3b25a6f
Hyochan <hyochan02@gmail.com>
2025-05-17 01:57:17 +0900

    style: 사진 업로드 이벤트 페이지 퍼블리싱

---
+++ src/components/events/photo-boards/SearchPhoto.tsx
@@ -0,0 +1,5 @@
+const SearchPhoto: React.FC = () => {
+  return <></>;
+};
+
+export default SearchPhoto;
---
+++ src/components/events/photo-boards/UploadPhoto.tsx
@@ -0,0 +1,53 @@
+import useBaseModal from '@/stores/baseModal';
+import { uploadPhotoPost } from '@/stores/events/BoardStore';
+import { useRef, useState } from 'react';
+
+const UploadPhoto: React.FC = () => {
+  const [uploading, setUploading] = useState(false);
+  const { openModal } = useBaseModal();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    try {
+      setUploading(true);
+      await uploadPhotoPost('');
+      openModal('uploadCompleteModal');
+    } catch (err: any) {
+      openModal('uploadFailModal')
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  return (
+    <div className="w-screen max-w-[500px] min-w-[375px] mx-auto">
+      <div className="w-full flex flex-col pt-20 px-4">
+        {/* 안내문구 */}
+        <p className="text-center text-base font-medium leading-relaxed whitespace-pre-line mb-8">
+          내가 찍은 축제 사진을 올려주세요!
+          {'\n'}(2025년 5월 26일 ~ 5월 29일 간)
+          {'\n'}좋아요를 가장 많이 받은 사람에게 경품을 드립니다!
+        </p>
+
+        {/* 업로드 박스 */}
+        <div
+          className="w-full aspect-[3/2] border border-dashed border-secondary-400 rounded-2xl flex items-center justify-center cursor-pointer"
+          onClick={() => inputRef.current?.click()}
+        >
+          <div className="flex flex-col items-center justify-center text-secondary-400">
+            <img src="/icons/events/photo.svg" alt="사진 추가" className="w-8 h-8 mb-2" />
+            <p className="text-base font-medium">{uploading ? '업로드 중...' : '사진 업로드'}</p>
+          </div>
+        </div>
+
+        {/* 숨겨진 파일 인풋 */}
+        <input type="file" accept="image/*" ref={inputRef} onChange={handleFileChange} className="hidden" />
+      </div>
+    </div>
+  );
+};
+
+export default UploadPhoto;
