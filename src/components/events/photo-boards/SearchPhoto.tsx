463c23031a6d18b6463bd64ef26fa70dd650e8e2
Hyochan <hyochan02@gmail.com>
2025-05-18 02:20:08 +0900

    style: 사진 카드 컴포넌트화 진행

--- src/components/events/photo-boards/SearchPhoto.tsx
+++ src/components/events/photo-boards/SearchPhoto.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { usePhotoStore } from '@/stores/events/BoardStore';
 import { getAllPhotos, getMyPhotos } from '@/stores/events/BoardStore';
 import { PhotoPost } from '@/types/Board.types';
+import PhotoCard from './PhotoCard';
 
 const SearchPhoto: React.FC = () => {
   const { myPhotos, myPhotoCount, allPhotos, allPhotoCount, setMyPhotos, setAllPhotos } = usePhotoStore();
@@ -33,7 +34,6 @@ const SearchPhoto: React.FC = () => {
     <div className="flex flex-col">
       <div className="w-screen max-w-[500px] min-w-[375px] z-0">
         <div className="pt-5 flex flex-col gap-5 dynamic-padding w-full justify-start">
-          {/* 내 사진 */}
           <p className="text-xl font-bold">내 사진</p>
           <div
             id="user-photo"
@@ -46,26 +46,13 @@ const SearchPhoto: React.FC = () => {
               }`}
             >
               {myPhotos.map((photo: PhotoPost) => (
-                <div
-                  key={photo.photoId}
-                  className="dynamic-item rounded-3xl bg-no-repeat bg-cover relative shrink-0"
-                  style={{ backgroundImage: `url(${photo.imageUrl})` }}
-                >
-                  <div className="flex flex-col justify-end text-white p-5 relative rounded-3xl dynamic-item">
-                    <div className="absolute inset-0 bg-gradient-to-t from-slate-700 via-slate-500 opacity-50 rounded-3xl"></div>
-                    <div className="relative z-10">
-                      <h2 className="font-bold mb-0.5 break-keep">{photo.mainUserName}</h2>
-                      <h2 className="text-2xs">❤️ {photo.heartCount}</h2>
-                    </div>
-                  </div>
-                </div>
+                <PhotoCard key={photo.photoId} photo={photo} isUserPhoto />
               ))}
             </div>
           </div>
 
           <hr className="border-black" />
 
-          {/* 업로드된 전체 사진 */}
           <p className="text-xl font-bold">업로드 된 사진</p>
           <div
             id="all-photo"
@@ -78,19 +65,7 @@ const SearchPhoto: React.FC = () => {
               }`}
             >
               {allPhotos.map((photo: PhotoPost) => (
-                <div
-                  key={photo.photoId}
-                  className="dynamic-item rounded-3xl bg-no-repeat bg-cover relative shrink-0"
-                  style={{ backgroundImage: `url(${photo.imageUrl})` }}
-                >
-                  <div className="flex flex-col justify-end text-white p-5 relative rounded-3xl dynamic-item">
-                    <div className="absolute inset-0 bg-gradient-to-t from-slate-700 via-slate-500 opacity-50 rounded-3xl"></div>
-                    <div className="relative z-10">
-                      <h2 className="font-bold mb-0.5 break-keep">{photo.mainUserName}</h2>
-                      <h2 className="text-2xs">❤️ {photo.heartCount}</h2>
-                    </div>
-                  </div>
-                </div>
+                <PhotoCard key={photo.photoId} photo={photo} isUserPhoto={false} />
               ))}
             </div>
           </div>
