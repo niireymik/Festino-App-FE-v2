8e21266d3e82ad57e619e660b6c6ece369c0bca5
Hyochan <hyochan02@gmail.com>
2025-05-17 19:58:42 +0900

    chore: 불필요 주석 제거

--- src/components/events/photo-boards/UploadPhoto.tsx
+++ src/components/events/photo-boards/UploadPhoto.tsx
@@ -15,8 +15,8 @@ const UploadPhoto: React.FC = () => {
       setUploading(true);
       await uploadPhotoPost('');
       openModal('uploadCompleteModal');
-    } catch (err: any) {
-      openModal('uploadFailModal')
+    } catch {
+      openModal('uploadFailModal');
     } finally {
       setUploading(false);
     }
@@ -25,14 +25,12 @@ const UploadPhoto: React.FC = () => {
   return (
     <div className="w-screen max-w-[500px] min-w-[375px] mx-auto">
       <div className="w-full flex flex-col pt-20 px-4">
-        {/* 안내문구 */}
         <p className="text-center text-base font-medium leading-relaxed whitespace-pre-line mb-8">
           내가 찍은 축제 사진을 올려주세요!
           {'\n'}(2025년 5월 26일 ~ 5월 29일 간)
           {'\n'}좋아요를 가장 많이 받은 사람에게 경품을 드립니다!
         </p>
 
-        {/* 업로드 박스 */}
         <div
           className="w-full aspect-[3/2] border border-dashed border-secondary-400 rounded-2xl flex items-center justify-center cursor-pointer"
           onClick={() => inputRef.current?.click()}
@@ -43,7 +41,6 @@ const UploadPhoto: React.FC = () => {
           </div>
         </div>
 
-        {/* 숨겨진 파일 인풋 */}
         <input type="file" accept="image/*" ref={inputRef} onChange={handleFileChange} className="hidden" />
       </div>
     </div>
