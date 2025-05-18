b212590bd0f83b676c31997d8bd65acdc63c0ac2
김민지 <alswlk0306@gmail.com>
2025-05-18 14:44:26 +0900

    fix: 예외처리 오류 수정

--- src/components/events/photo-boards/SearchPhoto.tsx
+++ src/components/events/photo-boards/SearchPhoto.tsx
@@ -13,21 +13,24 @@ const SearchPhoto: React.FC = () => {
     const fetchMyPhotos = async () => {
       try {
         const photo = await getMyPhotos('new');
-
-        if (!photo) {
+        console.log('myPhoto:', photo);
+    
+        if (!photo || !photo.photoList) {
           setMyPhotos([], 0);
         } else {
           setMyPhotos(photo.photoList, photo.photoTotalCount);
         }
       } catch (e) {
-        console.error(e);
+        console.error('내 사진 불러오기 실패:', e);
+        setMyPhotos([], 0); // 안전하게 초기화
       }
-    };
+    }    
 
     // 모든 사진 불러오기
     const fetchAllPhotos = async () => {
       try {
         const photo = await getAllPhotos('new');
+        console.log(photo)
 
         if (!photo) {
           setAllPhotos([], 0);
@@ -80,23 +83,29 @@ const SearchPhoto: React.FC = () => {
           <hr className="border-gray-300" />
 
           <p className="text-xl font-bold pl-2">모든 사진</p>
-          <div
-            id="all-photo"
-            className="w-full flex overflow-x-scroll scroll-smooth"
-            onTouchStart={(e) => e.stopPropagation()}
-          >
-            <div
-              className={`gap-x-2 gap-y-4 ${
-                allPhotoCount <= 2 ? 'flex justify-start' : 'grid place-content-start grid-cols-2 grid-flow-row'
-              }`}
-            >
-              {allPhotos
-                .filter(photo => photo.imageUrl) // imageUrl이 존재하는 경우만 필터링
-                .map((photo: PhotoPost) => (
-                  <PhotoCard key={photo.photoId} photo={photo} isUserPhoto={false} />
-              ))}
-            </div>
-          </div>
+            {allPhotoCount === 0 ? (
+              // 게시물이 존재하지 않을 때
+              <div className="w-full text-center text-primary-700">첫 번째 이벤트 참가자가 되어보세요!</div>
+            ) : (
+              // 게시물 존재
+              <div
+                id="all-photo"
+                className="w-full flex overflow-x-scroll scroll-smooth"
+                onTouchStart={(e) => e.stopPropagation()}
+              >
+                <div
+                  className={`gap-x-2 gap-y-4 ${
+                    allPhotoCount <= 2 ? 'flex justify-start' : 'grid place-content-start grid-cols-2 grid-flow-row'
+                  }`}
+                >
+                  {allPhotos
+                    .filter(photo => photo.imageUrl) // imageUrl이 존재하는 경우만 필터링
+                    .map((photo: PhotoPost) => (
+                      <PhotoCard key={photo.photoId} photo={photo} isUserPhoto={false} />
+                  ))}
+                </div>
+              </div>
+            )}
         </div>
       </div>
     </div>
