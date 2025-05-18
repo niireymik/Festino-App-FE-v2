d1d408e5c8ce9b20e483f53d1128081307161d62
Hyochan <hyochan02@gmail.com>
2025-05-19 02:44:05 +0900

    refactor: api.ts 변경에 따른 authStore 일부 로직 수정

--- src/stores/auths/authStore.ts
+++ src/stores/auths/authStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { baseApi } from '@/utils/api';
+import { api, baseApi } from '@/utils/api';
 import { AuthStore } from '@/types/Auth.types';
 import { formatPhoneNum, getCookie, removeCookie } from '@/utils/utils';
 import { setCookie } from '@/utils/utils';
@@ -29,24 +29,16 @@ export const useAuthStore = create<AuthStore>()((set, get) => ({
     const { openModal } = useBaseModal.getState();
 
     try {
-      const response = await baseApi.get('/main/user', {
+      const res = await baseApi.get('/main/user', {
         params: {
           'main-user-name': userName,
           'phone-num': formatPhoneNum(userPhoneNum),
         },
       });
 
-      const isSuccess = response.data.success;
-
-      if (!isSuccess) {
-        openModal('loginFailModal');
-        return false;
-      }
-
-      const mainUserId = response.data.data;
-
-      const accessToken = response.headers['access-token'];
-      const refreshToken = response.headers['refresh-token'];
+      const accessToken = res.headers['access-token'];
+      const refreshToken = res.headers['refresh-token'];
+      const mainUserId = res.data.data;
 
       if (accessToken) setCookie('accessToken', accessToken);
       if (refreshToken) setCookie('refreshToken', refreshToken);
@@ -56,7 +48,7 @@ export const useAuthStore = create<AuthStore>()((set, get) => ({
       localStorage.setItem('mainUserId', mainUserId);
       localStorage.setItem('userName', userName);
 
-      return isSuccess;
+      return res.data.success;
     } catch {
       openModal('loginFailModal');
       return false;
@@ -67,14 +59,16 @@ export const useAuthStore = create<AuthStore>()((set, get) => ({
     const { userName, userPhoneNum, userStudentNum, verifyCode } = get();
 
     try {
-      const response = await baseApi.post('main/user', {
+      const {
+        success,
+        message,
+        data: mainUserId,
+      } = await api.post('main/user', {
         mainUserName: userName,
         phoneNum: formatPhoneNum(userPhoneNum),
         studentNum: userStudentNum,
         authorizationCode: verifyCode,
       });
-
-      const { success, message, mainUserId } = response.data;
 
       if (success) {
         set({ mainUserId });
@@ -91,14 +85,16 @@ export const useAuthStore = create<AuthStore>()((set, get) => ({
     const { userName, userPhoneNum, userStudentNum, verifyCode } = get();
 
     try {
-      const response = await baseApi.post('/main/user/authorization', {
+      const {
+        success,
+        message,
+        data: mainUserId,
+      } = await api.post('/main/user/authorization', {
         mainUserName: userName,
         phoneNum: formatPhoneNum(userPhoneNum),
         studentNum: userStudentNum,
         authorizationCode: verifyCode,
       });
-
-      const { success, message, mainUserId } = response.data;
 
       if (success && mainUserId) {
         set({ mainUserId });
