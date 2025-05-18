e4bba3c6eb16e1285b34828092754169aacf9055
Hyochan <hyochan02@gmail.com>
2025-05-19 03:05:17 +0900

    refactor: api 로직 변경에 따른 timetableStore 일부 로직 수정

--- src/stores/homes/timetableStore.ts
+++ src/stores/homes/timetableStore.ts
@@ -1,14 +1,15 @@
 import { create } from 'zustand';
-import { baseApi } from '@/utils/api';
+import { api } from '@/utils/api';
 import { TimetableStore } from '@/types/ClubData.types';
 
 export const useTimetableStore = create<TimetableStore>((set) => ({
   clubData: [],
   selectedClub: null,
+
   getClubTimetable: async (day) => {
     try {
-      const res = await baseApi.get(`/main/club/all/date/${day}`);
-      set({ clubData: res.data.data });
+      const { data } = await api.get(`/main/club/all/date/${day}`);
+      set({ clubData: data });
     } catch (error) {
       console.error('getClubTimetable 실패:', error);
       set({ clubData: [] });
